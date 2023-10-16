41fced9a2c17f41ecff504c450f5bc87b9fe1d41
startinggamedev <beadsparadise344@gmail.com>
2023-10-15 23:09:38 -0300

    added physics class and remade line wandering

--- resources/states/physics/dead_physics.gd
+++ resources/states/physics/dead_physics.gd
@@ -1,1 +1,3 @@
-extends Node
+func physics_process():
+	self.set_linear_speed("dead_speed",self.spd_res.acceleration,Vector2(0,1),0.,100.,true)
+	physics
--- templates/graphics/camera_template.gd
+++ templates/graphics/camera_template.gd
@@ -4,5 +4,5 @@ func ready():
 	process_priority = globals.priority[0]
 func _process(delta):
 	global_position = Vector2(0.,0.)
-	var io = Vector2(get_viewport().get_visible_rect().size) / Vector2(320.,180. )
+	var io = Vector2(get_viewport().get_visible_rect().size) / Vector2(320 * 2.,180. * 2. )
 	camera_template.zoom = Vector2(io)
