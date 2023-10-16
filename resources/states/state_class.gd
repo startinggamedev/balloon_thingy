69e9adcae7e01f1551041e7409fb586b9debb4e0
startinggamedev <beadsparadise344@gmail.com>
2023-10-15 21:51:46 -0300

    commite before tryign to add physics tempalte class

---
+++ resources/states/state_class.gd
@@ -0,0 +1,4 @@
+class_name state
+@export var exit_conditions : Array[Script] 
+@export var process = Script
+@export var physics = Script
--- states/template_state.gd
+++
@@ -1,6 +0,0 @@
-class_name state
-func process():
-	self.d
-func physics():
-	pass
-@export var exit_condition = [Script]
